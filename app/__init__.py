bdcc29291ace7edaf5dd08906cb0713ccadcad20
jcainslie <jcainslie@hotmail.com>
2025-08-07 15:34:00 -0500

    Updates

--- app/__init__.py
+++ app/__init__.py
@@ -34,6 +34,5 @@ def create_app():
     app.register_blueprint(bills_bp)
     app.register_blueprint(auth_bp)
 
-    print("Starting app", flush=True)
 
     return app
--- run.py
+++ run.py
@@ -3,5 +3,4 @@ from app import create_app
 app = create_app()
 
 if __name__ == '__main__':
-    print("Calling app.run", flush=True)
     app.run(debug=True)
